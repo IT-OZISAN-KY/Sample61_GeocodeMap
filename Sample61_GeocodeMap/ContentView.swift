1268e6db27c2892af7f8c531be0601fa4628f699
k-yamaki <k.yamaki@hiroshimamm.com>
2022-01-21 09:17:31 +0900

    Initial Commit

---
+++ Sample61_GeocodeMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample61_GeocodeMap
+//
+//  Created by keiji yamaki on 2022/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample61_GeocodeMap/Sample61_GeocodeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample61_GeocodeMapApp.swift
+//  Sample61_GeocodeMap
+//
+//  Created by keiji yamaki on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Sample61_GeocodeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
